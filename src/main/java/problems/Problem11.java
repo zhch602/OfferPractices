7004890f99248aa0c7632b687096bc9a2d7e2730
zhch602 <494708977@qq.com>
2017-08-17 13:11:26 +0800

    二进制中1的个数

---
+++ src/main/java/problems/Problem11.java
@@ -0,0 +1,14 @@
+package problems;
+
+public class Problem11 {
+
+    public int NumberOf1(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = (n - 1) & n;
+        }
+        return count;
+    }
+
+}
