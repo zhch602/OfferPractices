40e002048eb94a805d9f1867fa74963dff2f67d7
zhch602 <494708977@qq.com>
2017-08-17 10:07:27 +0800

    矩形覆盖

---
+++ src/main/java/problems/Problem10.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class Problem10 {
+
+    public int RectCover(int target) {
+        if (target <= 2) {
+            return target;
+        }
+        int[] record = new int[target + 1];
+        record[0] = 1;
+        record[1] = 2;
+        for (int i = 2; i <= target; i++) {
+            record[i] = record[i - 1] + record[i - 2];
+        }
+        return record[target - 1];
+    }
+
+}
