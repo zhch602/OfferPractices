98f03c42bebbc57f256e56458770e5751aedc508
zhch602 <494708977@qq.com>
2017-08-18 13:23:36 +0800

    二叉树的镜像

---
+++ src/main/java/problems/Problem18.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class Problem18 {
+
+    public void Mirror(TreeNode root) {
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null)
+            return;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+
+}
