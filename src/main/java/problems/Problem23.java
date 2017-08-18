58a3dd23e721af5716072debcd8e45419e3ca61d
zhch602 <494708977@qq.com>
2017-08-18 17:14:05 +0800

    二叉搜索树的后序遍历序列

---
+++ src/main/java/problems/Problem23.java
@@ -0,0 +1,26 @@
+package problems;
+
+public class Problem23 {
+
+    public boolean VerifySquenceOfBST(int[] sequence) {
+        if (sequence.length == 0)
+            return false;
+        return IsTreeBST(sequence, 0, sequence.length - 1);
+    }
+
+    public boolean IsTreeBST(int[] sequence, int start, int end) {
+        if (end <= start)
+            return true;
+        int i = start;
+        for (; i < end; i++) {
+            if (sequence[i] > sequence[end])
+                break;
+        }
+        for (int j = i; j < end; j++) {
+            if (sequence[j] < sequence[end])
+                return false;
+        }
+        return IsTreeBST(sequence, start, i - 1) && IsTreeBST(sequence, i, end - 1);
+    }
+
+}
