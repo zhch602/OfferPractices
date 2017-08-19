305e4cd0eebe277c166ccd329d61d892598af93c
zhch602 <494708977@qq.com>
2017-08-19 16:07:34 +0800

    平衡二叉树

---
+++ src/main/java/problems/Problem39.java
@@ -0,0 +1,24 @@
+package problems;
+
+
+public class Problem39 {
+
+    public boolean IsBalanced_Solution(TreeNode root) {
+        if (root==null)
+            return true;
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        if (Math.abs(left-right)>1)
+            return false;
+        return IsBalanced_Solution(root.left)&&IsBalanced_Solution(root.right);
+    }
+
+    public int TreeDepth(TreeNode root) {
+        if (root==null)
+            return 0;
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return left>right?left+1:right+1;
+    }
+
+}
