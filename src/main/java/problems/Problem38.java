21a207c891fb9287ad5adb2ee747754633ab20f2
zhch602 <494708977@qq.com>
2017-08-19 16:01:37 +0800

    二叉树的深度

---
+++ src/main/java/problems/Problem38.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class Problem38 {
+
+    public int TreeDepth(TreeNode root) {
+        if (root==null)
+            return 0;
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return left>right?left+1:right+1;
+    }
+
+}
