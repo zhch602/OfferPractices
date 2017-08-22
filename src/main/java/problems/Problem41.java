69e87c0f4ed4de8bdbe0e02af5e2d365ca42cb5d
zhch602 <494708977@qq.com>
2017-08-22 17:04:51 +0800

    和为S的连续正数序列

--- src/main/java/problems/Problem41.java
+++ src/main/java/problems/Problem41.java
@@ -18,27 +18,27 @@ public class Problem41 {
 
     public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
         ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
-        if (sum<3)
+        if (sum < 3)
             return lists;
         int small = 1;
         int big = 2;
-        int middle = (1+sum)/2;
-        int curSum = small+big;
-        while (small<middle){
-            if (curSum==sum){
+        int middle = (1 + sum) / 2;
+        int curSum = small + big;
+        while (small < middle) {
+            if (curSum == sum) {
                 ArrayList<Integer> list = new ArrayList<Integer>();
                 for (int i = small; i <= big; i++) {
                     list.add(i);
                 }
                 lists.add(list);
                 big++;
-                curSum+=big;
-            }else if (curSum>sum){
-                curSum-=small;
+                curSum += big;
+            } else if (curSum > sum) {
+                curSum -= small;
                 small++;
-            }else {
+            } else {
                 big++;
-                curSum+=big;
+                curSum += big;
             }
         }
         return lists;
