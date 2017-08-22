83445a0ebcdb02da69f7fe6638fad6aa3dbe0480
zhch602 <494708977@qq.com>
2017-08-22 17:03:13 +0800

    和为S的连续正数序列

---
+++ src/main/java/problems/Problem41.java
@@ -0,0 +1,47 @@
+package problems;
+
+import java.util.ArrayList;
+
+public class Problem41 {
+
+    public static void main(String[] args) {
+        Problem41 problem41 = new Problem41();
+        ArrayList<ArrayList<Integer>> arrayLists = problem41.FindContinuousSequence(15);
+        for (ArrayList<Integer> arrayList : arrayLists) {
+            for (Integer integer : arrayList) {
+                System.out.println(integer);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
+        if (sum<3)
+            return lists;
+        int small = 1;
+        int big = 2;
+        int middle = (1+sum)/2;
+        int curSum = small+big;
+        while (small<middle){
+            if (curSum==sum){
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                for (int i = small; i <= big; i++) {
+                    list.add(i);
+                }
+                lists.add(list);
+                big++;
+                curSum+=big;
+            }else if (curSum>sum){
+                curSum-=small;
+                small++;
+            }else {
+                big++;
+                curSum+=big;
+            }
+        }
+        return lists;
+    }
+
+}
---
+++ src/main/java/problems/Problem42.java
@@ -0,0 +1,25 @@
+package problems;
+
+import java.util.ArrayList;
+
+public class Problem42 {
+
+    public ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int ahead = array.length-1;
+        int behind = 0;
+        while (ahead>behind){
+            int curSum = array[behind]+array[ahead];
+            if (curSum==sum){
+                list.add(array[behind]);
+                list.add(array[ahead]);
+                break;
+            }else if (curSum>sum)
+                ahead--;
+            else
+                behind++;
+        }
+        return list;
+    }
+
+}
