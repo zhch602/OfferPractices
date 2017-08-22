b01cf30982a450f9eade4a28544e72fa6a6eeebd
zhch602 <494708977@qq.com>
2017-08-22 17:05:12 +0800

    和为S的两个数字

--- src/main/java/problems/Problem42.java
+++ src/main/java/problems/Problem42.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public class Problem42 {
 
-    public ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
+    public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        int ahead = array.length-1;
+        int ahead = array.length - 1;
         int behind = 0;
-        while (ahead>behind){
-            int curSum = array[behind]+array[ahead];
-            if (curSum==sum){
+        while (ahead > behind) {
+            int curSum = array[behind] + array[ahead];
+            if (curSum == sum) {
                 list.add(array[behind]);
                 list.add(array[ahead]);
                 break;
-            }else if (curSum>sum)
+            } else if (curSum > sum)
                 ahead--;
             else
                 behind++;
