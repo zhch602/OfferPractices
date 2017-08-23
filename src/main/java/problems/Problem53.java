effb06b1a96995e92b4522da288e513ead3d9ec8
zhch602 <494708977@qq.com>
2017-08-23 15:50:48 +0800

    表示数值的字符串

---
+++ src/main/java/problems/Problem53.java
@@ -0,0 +1,10 @@
+package problems;
+
+public class Problem53 {
+
+    public boolean isNumeric(char[] str) {
+        String string = String.valueOf(str);
+        return string.matches("[\\+-]?[0-9]*(\\.[0-9]*)?([eE][\\+-]?[0-9]+)?");
+    }
+
+}
