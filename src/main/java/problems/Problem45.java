ea16b144348a0851800e97ed2896fef757111804
zhch602 <494708977@qq.com>
2017-08-23 10:03:40 +0800

    扑克牌顺子

---
+++ src/main/java/problems/Problem45.java
@@ -0,0 +1,26 @@
+package problems;
+
+import java.util.Arrays;
+
+public class Problem45 {
+
+    public boolean isContinuous(int[] numbers) {
+        if (numbers == null)
+            return false;
+        Arrays.sort(numbers);  //先排序
+        int zero = 0, i = 0;
+        for (; i < numbers.length && numbers[i] == 0; i++) {
+            zero++;  //统计0的个数
+        }
+        for (; i < numbers.length - 1 && zero >= 0; i++) {
+            if (numbers[i] == numbers[i + 1]) //有对子，则返回false
+                return false;
+            if (numbers[i] + 1 + zero >= numbers[i + 1]) {  //可以继续匹配
+                zero -= numbers[i + 1] - numbers[i] - 1;
+            } else
+                return false;
+        }
+        return i == numbers.length - 1;
+    }
+
+}
