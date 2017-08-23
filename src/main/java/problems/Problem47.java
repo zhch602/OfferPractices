d511fd5ff562327c24fe69b97158db125c89611c
zhch602 <494708977@qq.com>
2017-08-23 10:18:40 +0800

    求1+2+3+...+n

---
+++ src/main/java/problems/Problem47.java
@@ -0,0 +1,11 @@
+package problems;
+
+public class Problem47 {
+
+    public int Sum_Solution(int n) {
+        int sum = n;
+        boolean ans = (n > 0) && ((sum += Sum_Solution(n - 1)) > 0);
+        return sum;
+    }
+
+}
